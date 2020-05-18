4fc8cca47fd01ee9812dd9c2019468ede8b2cbcf
William Kwon <williamkwon97@gmail.com>
2020-05-17 19:33:46 -0500

    edited

--- FDA_Drug.py
+++ FDA_Drug.py
@@ -6,8 +6,30 @@ import csv
 html = urlopen(
     "https://www.accessdata.fda.gov/scripts/drugshortages/default.cfm")
 bsObj = BeautifulSoup(html, "lxml")
-table = bsObj.findAll("table", {"class": "wikitable"})[0]
-rows = table.findAll("tr") #Each table row 
+
+
+table = bsObj.find('table', id="cont")
+rows = table.findAll("tr")
+
+
+links =  [a['href'] for a in table.find_all('a', href=True) if a.text]
+new_links = []
+for link in links:
+    new_links.append(("https://www.accessdata.fda.gov/scripts/drugshortages/"+link).replace(" ", "%20"))
+
+
+for link in new_links:
+    
+    
+    html = urlopen(link)
+    bsObj_href = BeautifulSoup(html, "lxml")
+    div_href = bsObj_href.find("div",{"id":"accordion"})
+    table_href = bsObj_href.find("table",{"class":"table-bordered table-striped footable"})
+    print(table_href)
+    href_rows = table_href.findAll("tr")
+   
+
+
 csvFile = open("drug_shortage.csv", 'wt', newline='')
 writer = csv.writer(csvFile)
 try:
@@ -16,5 +38,6 @@ try:
         for cell in row.findAll(['td', 'th']):
             csvRow.append(cell.get_text())
         writer.writerow(csvRow)
+
 finally:
     csvFile.close()
