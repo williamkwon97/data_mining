358767124eb63bbac0a51d340a484dbbe906afe8
William Kwon <williamkwon97@gmail.com>
2020-05-18 01:14:11 -0500

    add href rows

--- FDA_Drug.py
+++ FDA_Drug.py
@@ -6,28 +6,21 @@ import csv
 html = urlopen(
     "https://www.accessdata.fda.gov/scripts/drugshortages/default.cfm")
 bsObj = BeautifulSoup(html, "lxml")
-
-
 table = bsObj.find('table', id="cont")
 rows = table.findAll("tr")
-
 
 links =  [a['href'] for a in table.find_all('a', href=True) if a.text]
 new_links = []
 for link in links:
     new_links.append(("https://www.accessdata.fda.gov/scripts/drugshortages/"+link).replace(" ", "%20"))
-
-
-for link in new_links:
-    
-    
+href_rows = []
+for link in new_links:   
+    link = link.replace("®", "%C2%AE")
     html = urlopen(link)
     bsObj_href = BeautifulSoup(html, "lxml")
+    #bsObj_href = BeautifulSoup (html.decode('utf-8', 'ignore'))
     div_href = bsObj_href.find("div",{"id":"accordion"})
-    table_href = bsObj_href.find("table",{"class":"table-bordered table-striped footable"})
-    print(table_href)
-    href_rows = table_href.findAll("tr")
-   
+    href_rows.append(div_href.findAll("tr"))
 
 
 csvFile = open("drug_shortage.csv", 'wt', newline='')
